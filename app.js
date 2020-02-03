9bddffa1c6527d70b10ab16a02c3ab5c586e263e
Luigi Di Paolo <luigidipaolo@Luigis-MacBook-Pro.local>
2020-02-03 14:02:17 +0000

    final version--

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const rate = document.querySelector("#rate");
 // Event listeners
 swap.addEventListener("click", swapper);
 inputFirst.addEventListener("click", calculateRate);
-inputSecond.addEventListener("clic", calculateRate);
+inputSecond.addEventListener("click", calculateRate);
 selectFirst.addEventListener("change", calculateRate);
 selectSecond.addEventListener("change", calculateRate);
 
